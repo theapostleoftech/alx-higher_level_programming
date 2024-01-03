53ac166bf15140101ed50aa699a387d585c6a414
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-01-02 17:35:18 -0800

    a script that creates a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[str.find("object"):str.find("object") + len("object-oriented programming with Python")])
