c5b05dee4c1206c834ad9042e225d9a0bce98ca3
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-01-03 20:42:42 +0100

    a program that prints all possible different combinations of two digits.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for digit in range(10):
     for _digit in range(digit + 1, 10):
-        print("{:d}{:d}{}".format(digit, _digit, ", "), end="")
+        print("{:d}{}".format(digit, _digit), end=",")
