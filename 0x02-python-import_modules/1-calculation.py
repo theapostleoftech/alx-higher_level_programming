71ae88800feb8adfd3a719c238ede35694dc6bd3
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-01-04 20:42:03 +0100

    a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import calculator_1 as calculate
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, calculate.add(a, b)))
-    print("{} - {} = {}".format(a, b, calculate.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculate.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculate.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
