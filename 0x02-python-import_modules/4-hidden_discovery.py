453d221cc1a4141a4b412048237582e7c0a66575
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-01-05 00:55:34 +0100

    a program that prints all the names defined by the compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3.8
+import importlib.util
+import sys
+
+
+def print_hidden_names(module_path):
+    spec = importlib.util.spec_from_file_location("hidden_4", module_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    names = [name for name in dir(module) if not name.startswith("__")]
+
+    for name in sorted(names):
+        print(name)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <module_path>".format(sys.argv[0]))
+        sys.exit(1)
+
+    module_path = sys.argv[1]
+    print_hidden_names(module_path)
