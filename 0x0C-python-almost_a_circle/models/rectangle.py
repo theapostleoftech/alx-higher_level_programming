a8c6bb461349ae9fdd6796c2ccd99d2e9dce2506
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-02-12 22:27:25 +0100

    Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,26 +102,56 @@ class Rectangle(Base):
                 if ar == 0:
                     self.id = arg
                 elif ar == 1:
-                    self.__width = arg
+                    self.width = arg
                 elif ar == 2:
-                    self.__height = arg
+                    self.height = arg
                 elif ar == 3:
-                    self.__x = arg
+                    self.x = arg
                 elif ar == 4:
-                    self.__y = arg
+                    self.y = arg
                 ar += 1
         else:
             for key, value in kwargs.items():
                 if key == "id":
                     self.id = value
                 elif key == "width":
-                    self.__width = value
+                    self.width = value
                 elif key == "height":
-                    self.__height = value
+                    self.height = value
                 elif key == "x":
-                    self.__x = value
+                    self.x = value
                 elif key == "y":
-                    self.__y = value
+                    self.y = value
+
+    # def update(self, *args, **kwargs):
+    #     """Assigns key/value arguments to attributes"""
+    #
+    #     if args and len(args) != 0:
+    #         ar = 0
+    #         for arg in args:
+    #             if ar == 0:
+    #                 self.id = arg
+    #             elif ar == 1:
+    #                 self.__width = arg
+    #             elif ar == 2:
+    #                 self.__height = arg
+    #             elif ar == 3:
+    #                 self.__x = arg
+    #             elif ar == 4:
+    #                 self.__y = arg
+    #             ar += 1
+    #     else:
+    #         for key, value in kwargs.items():
+    #             if key == "id":
+    #                 self.id = value
+    #             elif key == "width":
+    #                 self.__width = value
+    #             elif key == "height":
+    #                 self.__height = value
+    #             elif key == "x":
+    #                 self.__x = value
+    #             elif key == "y":
+    #                 self.__y = value
 
     def to_dictionary(self):
         """Returns the dictionary representation of the rectangle"""
