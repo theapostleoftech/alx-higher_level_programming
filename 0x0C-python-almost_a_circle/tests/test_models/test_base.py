c35db824835f3c262023751ceff05185dc561098
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-02-12 21:58:15 +0100

    Almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import inspect
 import json as js
 import unittest
 
@@ -117,6 +118,38 @@ class TestBase(unittest.TestCase):
         self.assertEqual(json_string, "[]")
 
     pass
+
+
+class TestSquare(unittest.TestCase):
+    """
+    class for testing Base class' methods
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up class method for the doc tests
+        """
+        cls.setup = inspect.getmembers(Base, inspect.isfunction)
+
+    def test_module_docstring(self):
+        """
+        Testing if module docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """
+        Testing if class docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """
+        Testing if methods docstring documentation exist
+        """
+        for func in self.setup:
+            self.assertTrue(len(func[1].__doc__) >= 1)
 
 
 if __name__ == '__main__':
