c28dcb6c5bb75c86e8a3bdeb53f21b2953cb15a5
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-03-13 09:01:42 -0700

    Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- This script lists all records with
+-- a score >= 10 in the table second_table
+-- of the database in your MySQL.
+SELECT score, name FROM second_table WHERE score >=10 ORDER BY score DESC
+
