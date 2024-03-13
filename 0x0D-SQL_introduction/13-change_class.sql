5ecce36eb8667f45729f8fcbf7dff9cbd591a85e
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-03-13 09:27:53 -0700

    Score too low

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- This script removes all records with
+-- a score <= 5 in the table second_table
+-- of the database in your MySQL server
+DELETE FROM second_table WHERE score <= 5 ORDER BY score DESC
+
