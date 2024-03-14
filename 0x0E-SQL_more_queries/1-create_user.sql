f4eb51dd727587b87de35105bb615e7e8767d83f
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-03-14 10:54:30 -0700

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- This script creates the MySQL user user_0d_1
 -- with all privileges given to it.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1-pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d-1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d-1'@'localhost'
+
