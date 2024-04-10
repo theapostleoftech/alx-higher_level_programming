0dd134ab144e628df8e18d002d61cd0519ebb583
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-10 13:34:54 +0100

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const Sz = parseInt(process.argv[2]);
+if (!+Sz) {
+    console.log('Missing size')
+} else if (Sz > 0) {
+    for (let i = 0; i < Sz; i++) {
+        console.log('X'.repeat(Sz));
+    }
+}
