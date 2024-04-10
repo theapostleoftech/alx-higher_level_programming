79e8c556c3b6dacc1b3aa41b055c43e59217f906
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-10 08:50:14 +0100

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 argv.forEach(index); {
-    if (index < 2) {
-        console.log('No argument');
-    } else if (index === 3) {
-        console.log('Argument found');
-    } else {
-        console.log('Arguments found');
-    }
-
+  if (index < 2) {
+    console.log('No argument');
+  } else if (index === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
 }
