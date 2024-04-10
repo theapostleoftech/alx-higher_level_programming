5463e4f5c70bea0fd35e6b29c2b93dff5e0207b6
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-10 18:21:25 +0100

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,9 +3,5 @@ const myArg = process.argv;
 if (myArg[2] === undefined) {
   console.log('No argument');
 } else {
-  myArg.slice(2).forEach((arg, index) => {
-    if (myArg[2]) {
-      console.log(`${arg}`);
-    }
-  });
+  console.log(myArg[2]);
 }
