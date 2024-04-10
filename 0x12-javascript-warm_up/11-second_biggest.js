5b30481e01102be3bd64bbd0652529121a034fad
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-10 16:13:15 +0100

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let numbers = process.argv.slice(2).map(Number);
+const numbers = process.argv.slice(2).map(Number);
 
 if (numbers.length <= 1) {
-    console.log(0);
+  console.log(0);
 } else {
-    numbers.sort((a, b) => b - a);
-    console.log(numbers[1]);
+  numbers.sort((a, b) => b - a);
+  console.log(numbers[1]);
 }
