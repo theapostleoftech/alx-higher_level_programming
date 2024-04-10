18ba841e756d0bfb15471a72f7044a18a5c06802
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-10 14:57:23 +0100

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const n = parseInt(process.argv[2]);
 let result;
-function factorial(n) {
-    if (!+n) {
-        return 1
-    } else if (n === 0 || n === 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+function factorial (n) {
+  if (!+n) {
+    return 1;
+  } else if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 result = factorial(n);
 console.log(result);
