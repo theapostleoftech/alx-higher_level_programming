9d7003bc670a938731989f3f1fc7d7e18e8f4489
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-11 01:51:40 +0100

    Rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
   constructor (w, h) {
-    if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      this.width = undefined;
-      this.height = undefined;
-    } else {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 }
-
 module.exports = Rectangle;
