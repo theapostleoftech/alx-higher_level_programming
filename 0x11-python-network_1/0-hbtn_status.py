2b38e91c7708d7c423ff4434f3d518f01e3c709f
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-26 23:21:05 -0700

    Whats my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,4 +3,11 @@
 This script fetches a URL
 """
 import urllib
-fetch_url = urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
+url_to_fetch = "https://alx-intranet.hbtn.io/status")
+with urllib.request.urlopen(url_to_fetch) as response:
+    html = response.read()
+
+print("Body response:")
+print("-\t type: {}".format(type(html)))
+print("-\t content: {}".format(html))
+print("-\t utf8 content: {}".format(html.decode("utf-8")))
