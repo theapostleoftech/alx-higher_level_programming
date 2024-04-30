f26d2c5236df590e6f2da513dc307f54fe8c588e
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-30 11:59:08 +0100

    Solutions to networking tasks

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display all HTTP methods the server will accept using curl.
-curl -sXI OPTIONS "$1" | grep "Allow" | cut -d " " -f2-
+curl -s -X OPTIONS -I "$1" | grep "Allow:" | cut -d ' ' -f2-
