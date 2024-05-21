5ea8c55cf59162bf0c207bca4d198afd9287a1a0
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-21 10:48:40 +0100

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// A script to read the contents of a file
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+const readFile = (path) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(data);
+        }
+    });
+};
+
+if (fileppath) {
+    readFile(filePath);
+} else {
+    console.error('Please provide a file path as an argument.');
+}
