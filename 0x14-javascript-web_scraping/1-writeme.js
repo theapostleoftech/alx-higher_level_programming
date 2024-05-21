fcb40052120f74cd556b76431758cab058f6462c
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-21 11:07:45 +0100

    Status code

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// A script that displays
+// the status code of a GET request
+
+const request = require('request');
+
+const url = process.argv[2];
+
+if (url) {
+    request(url, (error, response) => {
+        if (error) {
+            console.error('Error:', error);
+        } else {
+            console.log(`code: ${response.statusCode}`);
+        }
+    });
+} else {
+    console.error('Please provide a url as an argument.');
+}
