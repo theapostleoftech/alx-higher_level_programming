19d318714896127b42ffe3f641f66816e0b5ef52
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-21 15:01:31 +0100

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,17 +4,16 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-const stringToWrite = process.argv[3]
+const stringToWrite = process.argv[3];
 
 if (filePath && stringToWrite) {
-
-    fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-        if (err) {
-            console.error('Error:', err);
-        } else {
-            console.log('File written successfully');
-        }
-    });
+  fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      console.log('File written successfully');
+    }
+  });
 } else {
-    console.error('Please provide both the file path and the string to write as argument');
+  console.error('Please provide both the file path and the string to write as argument');
 }
