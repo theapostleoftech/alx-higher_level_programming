e7ce0f5e0430a2f87f786266e196b11a7721282f
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-21 11:37:05 +0100

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// A script that prints the title of
+// a Star Wars Movie episode
+const request = require('request');
+
+const movieId = process.argv[1];
+
+if (movieId) {
+    const api = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+    request(api, (error, response, body) => {
+        if (error) {
+            console.error('Error:', error);
+        } else {
+            const movie = JSON.parse(body);
+
+            console.log(movie.title);
+        }
+    });
+
+} else {
+    console.error('Please provide a movie ID as an argument.');
+}
