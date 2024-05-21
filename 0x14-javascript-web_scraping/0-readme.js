ca8460d628f3f1622dbedd6cad162a82340b9a10
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-21 15:00:54 +0100

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,17 +5,17 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 const readFile = (path) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(data);
-        }
-    });
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+    }
+  });
 };
 
 if (filePath) {
-    readFile(filePath);
+  readFile(filePath);
 } else {
-    console.error('Please provide a file path as an argument.');
+  console.error('Please provide a file path as an argument.');
 }
