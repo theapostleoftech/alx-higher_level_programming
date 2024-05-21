1066423b6b7a211bec9e27c53df2ce00f95a9df2
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-21 15:15:50 +0100

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// A script to write a web content to file
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+      } else {
+        console.log(`File saved: ${filePath}`);
+      }
+    });
+  }
+});
