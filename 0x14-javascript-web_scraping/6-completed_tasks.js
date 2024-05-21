fadc01749e08a6371f1c0a9583c8b442e3e7dcba
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-21 15:21:38 +0100

    How many completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+// A script to compute number of tasks completed by user id
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        const userId = todo.userId;
+        if (completedTasksByUser[userId]) {
+          completedTasksByUser[userId]++;
+        } else {
+          completedTasksByUser[userId] = 1;
+        }
+      }
+    });
+
+    const userIdsWithCompletedTasks = Object.keys(completedTasksByUser);
+    if (userIdsWithCompletedTasks.length === 0) {
+      console.log('No users with completed tasks.');
+    } else {
+      console.log(completedTasksByUser);
+    }
+  }
+});
