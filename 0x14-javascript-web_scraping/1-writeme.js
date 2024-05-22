a0eb3bd3c42bec5a29b393308fd01dc7d56624bd
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-22 10:49:33 +0100

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -10,10 +10,6 @@ if (filePath && stringToWrite) {
   fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
     if (err) {
       console.error('Error:', err);
-    } else {
-      console.log('File written successfully');
     }
   });
-} else {
-  console.error('Please provide both the file path and the string to write as argument');
 }
