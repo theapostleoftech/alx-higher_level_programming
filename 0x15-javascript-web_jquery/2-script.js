b4002f25dfeb024a7e5c721ef1ebaa03aacdf768
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-30 01:03:08 +0100

    Click and turn red

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+/*
+  This updates the text color of t<header> to red (#FF0000)
+  when the user clicks on the tag DIV#red_header
+*/
+$('DIV#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
+  
