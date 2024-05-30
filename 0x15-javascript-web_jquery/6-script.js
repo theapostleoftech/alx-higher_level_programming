1a5a802167447270c05a209fa6b06706d9944519
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-30 01:13:05 +0100

    Chnage the text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+/*
+  this updates the text of the <header> to “New Header!!!”
+  when the user clicks on DIV#update_header
+*/
+$('DIV#update_header').click(function () {
+  $('header').text('New Header!!!');
+});
