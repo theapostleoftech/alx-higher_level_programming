2a30431b897841f9eb11f59cc08e49272fd51c65
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-30 01:26:05 +0100

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+/*
+  this updates the text color of the <head> to red (#FF0000)
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
