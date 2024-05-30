4cf17daf69e01b854098d140c61350c58f395591
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-30 01:27:42 +0100

    Say Hello!

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+/*
+   this fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays
+   the value of hello from that fetch in the HTML’s tag DIV#hello
+*/
+const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+$(document).ready(() => {
+  $.get(url, function (data, textStatus) {
+    $('DIV#hello').text(data.hello);
+  });
+});
